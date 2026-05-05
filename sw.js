2015f2c18eb0c5d4f47e6238eb3517d553291dae
w59001109-arch <w59001109@gmail.com>
2026-05-05 15:13:25 +0800

    chore: SW v4 + skipWaiting/claim

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
-const CACHE_VER = 'v3';
+
+const CACHE_VER = 'v4';
 const urls = ['/Ac-/index.html'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_VER).then(c => c.addAll(urls))) });
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_VER).then(c => c.addAll(urls)));
+  self.skipWaiting();
+});
 self.addEventListener('fetch', e => {
   if (e.request.url.includes('index.html')) {
     e.respondWith(fetch(e.request).then(r => { caches.open(CACHE_VER).then(c => c.put(e.request, r.clone())); return r; }).catch(() => caches.match(e.request)));
@@ -8,4 +12,7 @@ self.addEventListener('fetch', e => {
     e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
   }
 });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(v=>v!==CACHE_VER).map(v=>caches.delete(v))))) });
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+  e.waitUntil(caches.keys().then(k => Promise.all(k.filter(v=>v!==CACHE_VER).map(v=>caches.delete(v)))));
+});
