fefe2f7065dedf83163ca06677fe82cc7eb5558f
w59001109-arch <w59001109@gmail.com>
2026-05-05 14:29:32 +0800

    SW always fetches fresh index.html, cache v3

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE_NAME = 'carbon-v2';
-const urls = ['/Ac-/','/Ac-/index.html','/Ac-/manifest.json','/Ac-/icon-192.png','/Ac-/icon-512.png'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urls))) });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))) });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(v=>v!=='carbon-v2').map(v=>caches.delete(v))))) });
+const CACHE_VER = 'v3';
+const urls = ['/Ac-/index.html'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_VER).then(c => c.addAll(urls))) });
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('index.html')) {
+    e.respondWith(fetch(e.request).then(r => { caches.open(CACHE_VER).then(c => c.put(e.request, r.clone())); return r; }).catch(() => caches.match(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(v=>v!==CACHE_VER).map(v=>caches.delete(v))))) });
