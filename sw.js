67665c8a0f0c5b5512916ce97e7190fcc03bb7f4
w59001109-arch <w59001109@gmail.com>
2026-05-05 14:24:37 +0800

    Bump SW cache to carbon-v2 to clear stale cache

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'carbon-v1';
+const CACHE_NAME = 'carbon-v2';
 const urls = ['/Ac-/','/Ac-/index.html','/Ac-/manifest.json','/Ac-/icon-192.png','/Ac-/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urls))) });
 self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))) });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(v=>v!=='carbon-v2').map(v=>caches.delete(v))))) });
